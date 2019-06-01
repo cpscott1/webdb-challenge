fef5463d303b91bcc181c6ba45e03f91a635d464
cpscott1 <thacam18@gmail.com>
2019-06-01 14:01:35 -0500

    added answers

--- projects/project-router.js
+++ projects/project-router.js
@@ -1,5 +1,52 @@
+const express = require('express');
+const db = require('../data/dbConfig.js');
+
 const router = require('express').Router();
 
-const db = require('../data/dbConfig.js');
+router.get('/', (req, res) => {
+  db('projects')
+  .then(projects => {
+    res.status(200).json(projects);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+})
+
+
+router.get('/project/:id', (req, res) => {
+  const { id } = req.params;
+
+    db('projects')
+    .where({ id: id })
+    .first()
+    .then(projects => {
+      db('actions')
+      .where({ project_id: id })
+      .then(actions => {
+        projects.actions = actions
+        return res.status(200).json(projects)
+      })
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    })
+  })
+
+
+router.post('/', (req, res) => {
+   db('projects').insert(req.body, 'id')
+   .then(ids => {
+     db('projects')
+     .where({ id: ids[0] })
+     .first()
+     .then(project => {
+       res.status(200).json(project)
+     })
+     .catch(err => {
+       res.status(500).json(err);
+     })
+   })
+})
 
 module.exports = router;
